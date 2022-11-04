762a979dbad31208af863e7412941f40db675149
Shong Lee <Shonglee17@gmail.com>
2022-11-04 14:08:08 -0500

    finished stretch questions

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -41,8 +41,8 @@
     console.log(friendsPets);
 
 // 11 - Add two pets to your `pets` variable
-    friendsPets+= 2;
-    console.log(friendsPets);
+    pets+= 2;
+    console.log(pets);
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
     const allowedPets = 2;
@@ -79,8 +79,8 @@
     }
     if (pets > allowedPets)
         {console.log('Oh no, I have too many pets!');
-    }
- small change
+    }// end of pets and pets allowed
+ 
 
 // STRETCH GOALS:
 
@@ -89,6 +89,17 @@
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+    let mostPets = ' most pets!';
+    if (pets > friendsPets)
+    {console.log('I have the' + mostPets);
+    }// end if I have more pets
+    else if (pets === friendsPets)
+    {console.log('We have the same amount of pets!');
+    }
+    else {console.log('My friend has the' + mostPets);
+    }// my friend has the most pets
+    
+
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -96,7 +107,25 @@
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+    switch(luckyNumber) {
+        case 1 : console.log('First is the worst')
+        break;
+
+        case 2 : console.log('Second is the best')
+        break;
+
+        case 3 : console.log('Third is the one with the polka dot dress')
+        break;
+
+        case 10 : console.log('Remember Remember the 10th of October')
+        break;
+
+        default : console.log('Luck is what happens when preparation meets opportunity')
+    }
+        
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+    adventurous = (true) ? console.log('Adventures are great!') : console.log('How about we stay home?');
+
 
 
